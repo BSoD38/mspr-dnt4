b890af518703f2add22c92663bfb4a85eb5fadae
Liam Powell <ljpbsod@gmail.com>
2021-02-25 11:20:46 +0100

    things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [files, setFiles] = useState([]);
+    const [curFile, setCurFile] = useState(null);
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [uploading, setUploading] = useState(false);
+
+    function addFile() {
+        setMessage("");
+        setError("");
+        if (!curFile || !curFile?.[0]?.name || files.includes(curFile[0].name)) {
+            setError("Vous n'avez pas sélectionné de fichier ou celui-ci existe déjà.");
+            return;
+        }
+        setUploading(true);
+        setTimeout(() => {
+            setFiles([...files, curFile[0].name]);
+            setCurFile(null);
+            setUploading(false);
+            setMessage("Le fichier a bien été transmis.")
+        }, 2000);
+    }
+
+    function onFileChange(event) {
+        setCurFile(event.target.files);
+    }
+
+    return (
+        <div>
+            <h1>EMESPERRE</h1>
+            <h2>Page de test</h2>
+            <div>
+                <label htmlFor="value">Téléversez un CSV</label>
+                <br/>
+                <input id="value" name="value" type="file" onChange={onFileChange} accept=".csv"/>
+                <br/>
+                <input type="submit" value="Envoyer" disabled={uploading} onClick={() => {addFile()}}/>
+            </div>
+            <div>
+                <ul>
+                    {files.map(file => <li key={file}>{file}</li>)}
+                </ul>
+            </div>
+            <h3 style={{color: "red"}}>{error}</h3>
+            <h3 style={{color: "darkgreen"}}>{message}</h3>
+        </div>
+    );
 }
 
 export default App;
