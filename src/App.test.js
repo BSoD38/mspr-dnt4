885ce2ca5c5e79565e39c3fbabb3bbc0f7bb9009
Liam Powell <ljpbsod@gmail.com>
2021-03-09 17:03:18 +0100

    change test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main app page with title', () => {
   render(<App />);
   const linkElement = screen.getByText(/EMESPERRE/i);
   expect(linkElement).toBeInTheDocument();
